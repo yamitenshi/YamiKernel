c92983e024a012fda2a535d4b09a7cd3fa560736
Nash van Gool <nashvangool@gmail.com>
2015-02-11 10:39:13 +0100

    Let the framebuffer code worry about bitshifting, only one definition of every color now

--- src/inc/colors.h
+++ src/inc/colors.h
@@ -1,38 +1,21 @@
 #ifndef COLORS_H_
 #define COLORS_H_
 
-#define FG_BLACK    0x00
-#define FG_BLUE     0x10
-#define FG_GREEN    0x20
-#define FG_CYAN     0x30
-#define FG_RED      0x40
-#define FG_MAGENTA  0x50
-#define FG_BROWN    0x60
-#define FG_LGREY    0x70
-#define FG_DGREY    0x80
-#define FG_LBLUE    0x90
-#define FG_LGREEN   0xA0
-#define FG_LCYAN    0xB0
-#define FG_LRED     0xC0
-#define FG_LMAGENTA 0xD0
-#define FG_LBROWN   0xE0
-#define FG_WHITE    0xF0
-
-#define BG_BLACK    0x00
-#define BG_BLUE     0x01
-#define BG_GREEN    0x02
-#define BG_CYAN     0x03
-#define BG_RED      0x04
-#define BG_MAGENTA  0x05
-#define BG_BROWN    0x06
-#define BG_LGREY    0x07
-#define BG_DGREY    0x08
-#define BG_LBLUE    0x09
-#define BG_LGREEN   0x0A
-#define BG_LCYAN    0x0B
-#define BG_LRED     0x0C
-#define BG_LMAGENTA 0x0D
-#define BG_LBROWN   0x0E
-#define BG_WHITE    0x0F
+#define FB_BLACK    0
+#define FB_BLUE     1
+#define FB_GREEN    2
+#define FB_CYAN     3
+#define FB_RED      4
+#define FB_MAGENTA  5
+#define FB_BROWN    6
+#define FB_LGREY    7
+#define FB_DGREY    8
+#define FB_LBLUE    9
+#define FB_LGREEN   10
+#define FB_LCYAN    11
+#define FB_LRED     12
+#define FB_LMAGENTA 13
+#define FB_LBROWN   14
+#define FB_WHITE    15
 
 #endif
