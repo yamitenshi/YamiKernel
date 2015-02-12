9262fef2ab90f24bc937fedb9ac46f9118e0eecf
Nash van Gool <nashvangool@gmail.com>
2015-02-12 09:31:44 +0100

    Added I/O code to read byte from port

--- src/io.s
+++ src/io.s
@@ -1,4 +1,5 @@
 global outb
+global inb
 
 ; Send a byte to the I/O port
 ; stack: [esp + 8] the data byte
@@ -8,4 +9,9 @@ outb:
     mov al, [esp + 8]
     mov dx, [esp + 4]
     out dx, al
+    ret
+
+inb
+    mov dx, [esp + 4]
+    in al, dx
     ret
